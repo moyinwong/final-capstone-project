f9bb83c6220272d03df3d141bdb5b2e36308e248
klchow <klchow1996@gmail.com>
2020-09-18 02:05:53 +0800

    Stack navigator re-structured

--- react-native-app/screens/bottomTap/categoryStack/category.tsx
+++ react-native-app/screens/bottomTap/categoryStack/category.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
 // Styles
 import globalStyles from '../../../styles/globalStyles';
 
-export default function Category() {
-    const navigation = useNavigation();
+export default function Category(props: { navigation: { navigate: (arg0: string) => void; }; }) {
 
     return (
         <View style={globalStyles.container}>
             <Text>Category Screen</Text>
             <Button
                 title="Go to Course"
-                onPress={() => navigation.navigate('Course')}
+                onPress={() => props.navigation.navigate('Course')}
             />
         </View>
     )
