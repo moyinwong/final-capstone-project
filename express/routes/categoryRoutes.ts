33d704a67ea1e9cc34c94f4dd1cfb957cbb84560
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-09-21 21:57:14 +0800

    basic carousel

--- express/routes/categoryRoutes.ts
+++ express/routes/categoryRoutes.ts
@@ -5,8 +5,4 @@ export const categoryRoutes = express.Router();
 
 categoryRoutes.get("/:name", categoryController.categoryCourses);
 categoryRoutes.get("/others/:name", categoryController.subcategoryCourses);
-<<<<<<< HEAD
 
-=======
-//categoryRoutes.get("/all", categoryController.allCourses);
->>>>>>> eb5ab3068d56df61bb4aaa344d4f53a26fa36b62
