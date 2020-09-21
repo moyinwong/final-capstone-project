20bd6a7c02feda45c7a4c2e5e82b21e913326aa9
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-09-21 10:24:14 +0800

    working on homepage

--- react-app/src/pages/HomePage.tsx
+++ react-app/src/pages/HomePage.tsx
@@ -7,12 +7,77 @@ import BurgerMenu from "../components/BurgerMenu";
 import { IRootState } from "../redux/store";
 import { logout } from "../redux/auth/actions";
 import Header from "../components/Header";
+import { Container, Row, Col } from "react-bootstrap";
+import './Homepage.scss'
 
 const HomePage = () => {
 
   return (
     <div>
-      
+      <section className="body-section-intro">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md={12} className="section-title">中學生線上學習平台</Col>
+            <Col md={8}>
+            Educate是一個針對DSE而設的網上補習平台，
+            透過融合科技和教育元素，讓同學足不出戶就能以十倍效率、一半時間、
+            針對自己的學習需要極速進步，輕鬆應付DSE！(copy from afterschool remember to change!!!!!)
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="section-card">
+                <div>
+                  <img className="section-icon" src={require("./icons/clock.png")} />
+                </div>
+                <div>
+                  <div>靈活學習</div>
+                  <div>按自己的節奏隨時隨地學習，靈活運用時間</div>
+                </div>
+              </div>
+            </Col>
+            
+            <Col>
+              <div className="section-card">
+                <div>
+                  <img className="section-icon" src={require("./icons/goal.png")} />
+                </div>
+                <div>
+                  <div>多種課程</div>
+                  <div>多種不同課程助你擴闊知識，達到目標</div>
+                </div>
+              </div>
+            </Col>
+
+          </Row>
+          <Row>
+            <Col>
+              <div className="section-card">
+                <div>
+                  <img className="section-icon" src={require("./icons/lectern.png")} />
+                </div>
+                <div>
+                  <div>專業導師團隊</div>
+                  <div>嚴選導師，教學經驗豐富，教學生動易明</div>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="section-card">
+                <div>
+                  <img className="section-icon" src={require("./icons/pencil-holder.png")} />
+                </div>
+                <div>
+                  <div>學習效率</div>
+                  <div>隨時重溫課堂錄影及筆記，提高學習效率</div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+              
+        </Container>
+      </section>
+
       <h1>This is HomePage</h1>
 
       <Responsive />
