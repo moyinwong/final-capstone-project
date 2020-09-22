ca21ec55769e1347538002176d37b943e35f5195
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-09-22 23:26:29 +0800

    user profile button

--- react-app/src/components/DropdownMenu.tsx
+++ react-app/src/components/DropdownMenu.tsx
@@ -4,10 +4,11 @@ import { useDispatch } from 'react-redux';
 import { logout } from '../redux/auth/actions';
 
 const DropdownMenu = () => {
+    
     const dispatch = useDispatch();
     return (
         <div className="user-dropdown-menu">
-            <a href="#" className="menu-item">My Profile</a>
+            <a href="#" className="menu-item">My profile</a>
             <a href="#" className="menu-item">My courses</a>
             <a href="#" className="menu-item">Account settings</a>
             <a href="#" className="menu-item">Messages</a>
@@ -16,6 +17,7 @@ const DropdownMenu = () => {
                   dispatch(logout());
                 }} href="#" className="menu-item">Logout</a>
         </div>
+        // <div>hello</div>
     )
 }
 
