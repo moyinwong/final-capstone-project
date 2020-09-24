481c5801617bbf97c72c80acc38953e018f98a93
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-09-24 12:27:28 +0800

    finished front-end multistep form & continue on backend

--- react-app/src/pages/CourseCreatePage.tsx
+++ react-app/src/pages/CourseCreatePage.tsx
@@ -4,18 +4,22 @@ import './CourseCreatePage.scss'
 import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik'
 import { TextField, Select, SimpleFileUpload } from 'formik-material-ui'
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardContent, MenuItem, FormControl, InputLabel, Button } from '@material-ui/core'
+import { Card, CardContent, MenuItem, FormControl, 
+    InputLabel, Button, Box, Stepper, Step, StepLabel, CircularProgress} from '@material-ui/core'
 import * as Yup from 'yup'
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
     //   margin: theme.spacing(1),
-      minWidth: 120,
+      minWidth: '50%',
     },
     selectEmpty: {
       marginTop: theme.spacing(2),
     },
   }));
+
+const sleep = (time: number) => new Promise((acc) => setTimeout(acc, time))
+
 
 const CourseCreatePage = () => {
     const classes = useStyles();
@@ -25,13 +29,11 @@ const CourseCreatePage = () => {
         .min(3, 'Too Short!')
         .max(20, 'Too Long!')
         .required('Required'),
+        courseCategory: Yup.string()
+        .required('Required')
     })
 
     const secondValidationSchema = Yup.object().shape({
-        courseCategory: Yup.string().required('Required')
-    })
-
-    const thirdValidationSchema = Yup.object().shape({
         coursePrice: Yup.number()
         .min(0, 'Min price is 0')
         .required('Required'),
@@ -45,7 +47,7 @@ const CourseCreatePage = () => {
         .required('Required'),
     })
 
-    const fourthValidationSchema = Yup.object().shape({
+    const thirdValidationSchema = Yup.object().shape({
         file: Yup.mixed().required('A file is required')
     })
   
@@ -53,7 +55,9 @@ const CourseCreatePage = () => {
         <div>
             <div className="course-creation-header">
                 {/* <img className="website-logo" src={require("../logo.png")} /> */}
-                <Button href="/instructor" variant="contained" color="secondary" size="large">Exit</Button>
+                <Box paddingTop={2}>
+                    <Button href="/instructor" variant="contained" color="secondary" size="large">Exit</Button>
+                </Box>
             </div>
 
             <Card>
@@ -68,44 +72,82 @@ const CourseCreatePage = () => {
                             file: null,
                             
                         }} 
-                        onSubmit={() => {}}
+                        onSubmit={async (values) => {
+                            await sleep(3000)
+                            console.log('values', values);
+                        }}
                     >
 
-                            <FormikStep validationSchema={firstValidationSchema}>
-                                <Field name="courseTitle" component={TextField} placeholder="e.g. 中文5**攻略" label="課程名稱" />
-                            </FormikStep>
-                            <FormikStep validationSchema={secondValidationSchema}>
-                                <FormControl className={classes.formControl}>
-                                    <InputLabel htmlFor="courseCategory">課程類別</InputLabel>
+                            <FormikStep label="課程名稱及種類" validationSchema={firstValidationSchema}>
+                                <Box paddingBottom={2}>
                                     <Field 
-                                        name="courseCategory" 
-                                        component={Select} 
-                                        inputProps={{
-                                            id: 'courseCategory',
-                                        }}
-                                    >
-                                        <MenuItem value={1}>中文</MenuItem>
-                                        <MenuItem value={2}>英文</MenuItem>
-                                        <MenuItem value={3}>數學</MenuItem>
-                                        <MenuItem value={4}>通識</MenuItem>
-                                        <MenuItem value={5}>物理</MenuItem>
-                                        <MenuItem value={6}>化學</MenuItem>
-                                        <MenuItem value={7}>生物</MenuItem>
-                                        <MenuItem value={8}>經濟</MenuItem>
-                                        <MenuItem value={9}>歷史</MenuItem>
-                                        <MenuItem value={10}>企會財</MenuItem>
-                                        <MenuItem value={11}>ICT</MenuItem>
-                                        <MenuItem value={12}>視覺藝術</MenuItem>
-                                        <MenuItem value={13}>M1</MenuItem>
-                                        <MenuItem value={14}>M2</MenuItem>
-                                        <MenuItem value={15}>其他</MenuItem>
-                                    </Field>
-                                </FormControl>
+                                        name="courseTitle" 
+                                        component={TextField} 
+                                        placeholder="e.g. 中文5**攻略" 
+                                        label="課程名稱" 
+                                        className={classes.formControl}
+                                    />
+                                </Box>
+                                <Box paddingBottom={2}>
+                                    <FormControl className={classes.formControl}>
+                                        <InputLabel htmlFor="courseCategory">課程類別</InputLabel>
+                                        <Field
+                                            fullidth
+                                            name="courseCategory" 
+                                            component={Select} 
+                                            inputProps={{
+                                                id: 'courseCategory',
+                                            }}
+                                        >
+                                            <MenuItem value={1}>中文</MenuItem>
+                                            <MenuItem value={2}>英文</MenuItem>
+                                            <MenuItem value={3}>數學</MenuItem>
+                                            <MenuItem value={4}>通識</MenuItem>
+                                            <MenuItem value={5}>物理</MenuItem>
+                                            <MenuItem value={6}>化學</MenuItem>
+                                            <MenuItem value={7}>生物</MenuItem>
+                                            <MenuItem value={8}>經濟</MenuItem>
+                                            <MenuItem value={9}>歷史</MenuItem>
+                                            <MenuItem value={10}>企會財</MenuItem>
+                                            <MenuItem value={11}>ICT</MenuItem>
+                                            <MenuItem value={12}>視覺藝術</MenuItem>
+                                            <MenuItem value={13}>M1</MenuItem>
+                                            <MenuItem value={14}>M2</MenuItem>
+                                            <MenuItem value={15}>其他</MenuItem>
+                                        </Field>
+                                    </FormControl>
+                                </Box>
                             </FormikStep>
-                            <FormikStep validationSchema={thirdValidationSchema}>
-                                <Field name="coursePrice" type="number" component={TextField} placeholder="100" label="課程價錢" />
-                                <Field name="courseDescription" component={TextField} placeholder="e.g. 帶你中文拎5**！" label="課程大綱" />
-                                <Field name="coursePrerequisite" component={TextField} placeholder="e.g. 中六程度中文" label="課程要求" />
+
+                            <FormikStep label="課程資料" validationSchema={secondValidationSchema}>
+                                <Box paddingBottom={2}>
+                                    <Field 
+                                        className={classes.formControl}
+                                        name="coursePrice" 
+                                        type="number" 
+                                        component={TextField} 
+                                        placeholder="100" 
+                                        label="課程價錢" 
+                                    />
+                                </Box>
+                                <Box paddingBottom={2}>
+                                    <Field 
+                                        className={classes.formControl}
+                                        name="courseDescription" 
+                                        component={TextField} 
+                                        placeholder="e.g. 帶你中文拎5**！" 
+                                        label="課程大綱" 
+                                    />
+                                </Box>
+                                <Box paddingBottom={2}>
+                                    <Field 
+                                        className={classes.formControl}
+                                        name="coursePrerequisite" 
+                                        component={TextField} 
+                                        placeholder="e.g. 中六程度中文" 
+                                        label="課程要求" 
+                                    />
+                                </Box>
                             </FormikStep>
                             {/* <input id="file" name="file" type="file" onChange={(event) => {
                                 let target = event.currentTarget as HTMLInputElement;
@@ -118,9 +160,16 @@ const CourseCreatePage = () => {
                                 props.setFieldValue("file", files[0]);
         
                             }} /> */}
-                            <div>
-                                <Field component={SimpleFileUpload} name="file" label="上戴課程封面" />
-                            </div>
+                            <FormikStep label="課程封面" validationSchema={thirdValidationSchema}>
+                                <Box paddingBottom={2}>
+                                    <Field
+                                        className={classes.formControl}
+                                        component={SimpleFileUpload} 
+                                        name="file" 
+                                        label="上戴課程封面" 
+                                    />
+                                </Box>
+                            </FormikStep>
 
 
 
@@ -134,17 +183,20 @@ const CourseCreatePage = () => {
 
 export default CourseCreatePage
 
-export interface FormikStepProps extends Pick<FormikConfig<FormikValues>, "children" | 'validationSchema'> {}
+export interface FormikStepProps 
+extends Pick<FormikConfig<FormikValues>, "children" | 'validationSchema'> {
+    label: string;
+}
 
 export function FormikStep({ children }: FormikStepProps) {
     return <div>{children}</div>
 }
 
 export function FormikStepper({children, ...props}: FormikConfig<FormikValues>) {
-    const childrenArray = React.Children.toArray(children) 
+    const childrenArray = React.Children.toArray(children) as React.ReactElement<FormikStepProps>[]
     const [step, setStep] = useState(0);
     const currentChild = childrenArray[step] as React.ReactElement<FormikStepProps>;
-    console.log(currentChild)
+    const [completed, setCompleted] = useState(false)
 
     function isLastStep() {
         return step === childrenArray.length - 1
@@ -157,22 +209,40 @@ export function FormikStepper({children, ...props}: FormikConfig<FormikValues>) 
             onSubmit={async (values, helpers) => {
             if (isLastStep()){
                 await props.onSubmit(values, helpers)
+                setCompleted(true);
+                // helpers.resetForm();
+                // setStep(0);
             } else {
                 setStep(step => step + 1)
             }}}
         >
-
-
-        <Form autoComplete="off">
-            {currentChild}
-            {step > 0 ? <Button onClick={() => {
-                setStep(step => step - 1)
-                console.log(step)
-                }}>Back</Button> : null}
-            <Button type="submit" onClick={() => {
-                console.log(step)
-                }}>{isLastStep() ? 'Submit' : 'Next'}</Button>
-        </Form>
+        
+        {({ isSubmitting }) => (
+            <Form autoComplete="off">
+                <Stepper alternativeLabel activeStep={step}>
+                    {childrenArray.map((child, index) => (
+                    <Step key={child.props.label} completed={step > index || completed}>
+                        <StepLabel>{child.props.label}</StepLabel>
+                    </Step>
+                    ))}
+                </Stepper>
+                {currentChild}
+                {step > 0 ? (
+                    <Button disabled={isSubmitting} onClick={() => setStep(step => step - 1)}>
+                        Back
+                    </Button>
+                ) : null}
+                <Button 
+                    startIcon={isSubmitting ? <CircularProgress size="1rem"/> : null} 
+                    disabled={isSubmitting} 
+                    type="submit" 
+                    variant="contained" 
+                    color="primary"
+                >
+                    {isSubmitting ? 'Submitting' : isLastStep() ? 'Submit' : 'Next'}
+                </Button>
+            </Form>
+        )}
 
 
         </Formik>
