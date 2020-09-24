dff58f005f0e10880b087fdcd9326bed17806aad
klchow <klchow1996@gmail.com>
2020-09-24 16:54:52 +0800

    Stars edited

--- react-native-app/sharedComponents/stars.tsx
+++ react-native-app/sharedComponents/stars.tsx
@@ -12,34 +12,28 @@ export default function Stars(props: { score: number; }) {
 
     let score = props.score;
 
-    const [firstStar, setFirstStar] = useState(score > 1 ? '100%' : (score * 100) + '%');
-    const [secondStar, setSecondStar] = useState(score < 1 ? '0%' : (score > 2 ? '100%' : ((score - 1) * 100) + '%'));
-    const [thirdStar, setThirdStar] = useState(score < 2 ? '0%' : (score > 3 ? '100%' : ((score - 2) * 100) + '%'));
-    const [forthStar, setForthStar] = useState(score < 3 ? '0%' : (score > 4 ? '100%' : ((score - 3) * 100) + '%'));
-    const [fifthStar, setFifthStar] = useState(score < 4 ? '0%' : ((score - 4) * 100) + '%');
+    const [starsWidth, setStarsWidth] = useState(
+        [
+            { id: '1', width: score > 1 ? '100%' : (score * 100) + '%' },
+            { id: '2', width: score < 1 ? '0%' : (score > 2 ? '100%' : ((score - 1) * 100) + '%') },
+            { id: '3', width: score < 2 ? '0%' : (score > 3 ? '100%' : ((score - 2) * 100) + '%') },
+            { id: '4', width: score < 3 ? '0%' : (score > 4 ? '100%' : ((score - 3) * 100) + '%') },
+            { id: '5', width: score < 4 ? '0%' : ((score - 4) * 100) + '%' }
+        ]
+    );
 
     return (
         <View style={starsStyles.starsContainer}>
-            <View>
-                <FontAwesome style={{ ...starsStyles.star, width: firstStar }} name="star" size={20} color="#fadd4d" />
-                <FontAwesome style={starsStyles.starEmpty} name="star" size={20} color="#cfd9ea" />
-            </View>
-            <View>
-                <FontAwesome style={{ ...starsStyles.star, width: secondStar }} name="star" size={20} color="#fadd4d" />
-                <FontAwesome style={starsStyles.starEmpty} name="star" size={20} color="#cfd9ea" />
-            </View>
-            <View>
-                <FontAwesome style={{ ...starsStyles.star, width: thirdStar }} name="star" size={20} color="#fadd4d" />
-                <FontAwesome style={starsStyles.starEmpty} name="star" size={20} color="#cfd9ea" />
-            </View>
-            <View>
-                <FontAwesome style={{ ...starsStyles.star, width: forthStar }} name="star" size={20} color="#fadd4d" />
-                <FontAwesome style={starsStyles.starEmpty} name="star" size={20} color="#cfd9ea" />
-            </View>
-            <View>
-                <FontAwesome style={{ ...starsStyles.star, width: fifthStar }} name="star" size={20} color="#fadd4d" />
-                <FontAwesome style={starsStyles.starEmpty} name="star" size={20} color="#cfd9ea" />
-            </View>
+
+            {starsWidth.map((item) => {
+                return (
+                    <View>
+                        <FontAwesome style={{ ...starsStyles.star, width: item.width }} name="star" size={20} color="#fadd4d" />
+                        <FontAwesome style={starsStyles.starEmpty} name="star" size={20} color="#cfd9ea" />
+                    </View>
+                )
+            })}
+
         </View>
     )
 }
