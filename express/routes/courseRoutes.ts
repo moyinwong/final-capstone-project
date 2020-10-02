621a800266c91864cfd7512ebd822363ed5016ab
Stanley Lau <siuclau4@gmail.com>
2020-10-02 12:01:34 +0800

    fixed good comment course route

--- express/routes/courseRoutes.ts
+++ express/routes/courseRoutes.ts
@@ -7,13 +7,13 @@ courseRoutes.post(
   "/create/:userEmail",
   upload.single("file"),
   courseController.courseCreation
-  );
-  
+);
+
 courseRoutes.get("/all/lessons/:courseId", courseController.getAllLessons);
 courseRoutes.get("/completion", courseController.checkCompletion);
 courseRoutes.get("/all/:tutor", courseController.courseByInstructor);
 courseRoutes.get("/popular", courseController.popularCourses);
-courseRoutes.get("/good-comment", courseController.popularCourses);
+courseRoutes.get("/good-comment", courseController.goodCommentCourses);
 courseRoutes.get("/:course", courseController.courseDetailInfoByName);
 courseRoutes.get("/:course/comment", courseController.courseComments);
 courseRoutes.put("/comment/update", isLoggedIn, courseController.updateComment);
