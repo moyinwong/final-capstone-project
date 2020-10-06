9952f30c29a94f4ec79673678621fb83fa0bcd44
klchow <klchow1996@gmail.com>
2020-10-06 11:34:34 +0800

    Cart Sum & Check duplicate Logic fixed

--- react-native-app/contexts/cartContext.tsx
+++ react-native-app/contexts/cartContext.tsx
@@ -1,5 +1,6 @@
 // React, React Native
 import React, { createContext, useState } from 'react';
+import { Alert } from 'react-native';
 
 // Interface
 import ICourse from '../Interfaces/ICourse';
@@ -15,15 +16,31 @@ const CartContextProvider = (props: any) => {
     const [cartSum, setCartSum] = useState(0);
 
     const addCartList = (course: ICourse) => {
-        let tempSum = 0;
-        for (let item of cartList) {
-            tempSum += (parseInt(item.price));
+        // Check
+        if (checkDuplicate(course)) {
+            Alert.alert(
+                "重複加入",
+                "此課程已在購物車",
+                [
+                    {
+                        text: "取消",
+                        onPress: () => console.log("取消"),
+                        style: "cancel"
+                    }
+                ],
+                { cancelable: true }
+            )
+        } else {
+            let tempSum = 0;
+            for (let item of cartList) {
+                tempSum += (parseInt(item.price));
+            }
+            tempSum += (parseInt(course.price));
+
+            setCartList([...cartList, course]);
+
+            setCartSum(tempSum);
         }
-        tempSum += (parseInt(course.price));
-
-        setCartList([...cartList, course]);
-
-        setCartSum(tempSum);
     };
     const removeCartList = (course: ICourse) => {
         let tempSum = 0;
@@ -36,9 +53,17 @@ const CartContextProvider = (props: any) => {
 
         setCartSum(tempSum);
     };
+    const checkDuplicate = (course: ICourse) => {
+        for (let item of cartList) {
+            if (course.id == item.id) {
+                return true
+            }
+        }
+        return false
+    }
 
     return (
-        <CartContext.Provider value={{ cartList, cartSum, addCartList, removeCartList, setCartList, setCartSum }}>
+        <CartContext.Provider value={{ cartList, cartSum, addCartList, removeCartList, checkDuplicate, setCartList, setCartSum }}>
             { props.children}
         </CartContext.Provider>
     )
